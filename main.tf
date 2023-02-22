070a5ee33f41a821f1874c60b93772a0a23a69d9
reshmagcp <reshmagcp2022@gmail.com>
2023-02-22 15:29:32 +0100

    gcp service account

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/Users/reshma/Downloads/terraform-378613-0e5fce9e53b6.json")
+
+  project = "terraform-378613"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
