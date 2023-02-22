f1da0abb42110f1e09f7ae5a3a629160cfa85b5e
reshmagcp <reshmagcp2022@gmail.com>
2023-02-22 16:42:15 +0100

    vm created with count 3 and destroyed

--- main.tf
+++ main.tf
@@ -19,3 +19,23 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-inst-${count.index}"
+  machine_type = "f1-micro"
+  count = 3
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+
